22250d0780b8b7879a7d52d3694692de484ed12d
felipesimao1 <felipe.simao1@gmail.com>
2024-03-11 12:31:37 +0000

    Applying return message js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,16 @@ document.querySelector(".scroll-btn").addEventListener("click", () => {
     document.querySelector("html").style.scrollBehavior = "unset";
   }, 500);
 });
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  var fullname = document.getElementById('fullname').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  // Validando se os campos estão preenchidos
+  if (!fullname || !email || !message) {
+      event.preventDefault(); // Impede o envio do formulário
+  } else {
+      alert('Mensagem enviada com sucesso!');
+  }
+});
